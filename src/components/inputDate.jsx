6ffa82d0d95ed1cb0c2000796d533c318b41fb97
Renaud <renaudfradin@gmail.com>
2023-05-14 19:50:18 +0200

    update : style form

--- src/components/inputDate.jsx
+++ src/components/inputDate.jsx
@@ -2,11 +2,15 @@ import React from "react";
 
 export default function inputDate() {
   return (
-    <>
-      <input type="text"
-        className="inputSearch"
-        placeholder="dfate"
+    <div className="boxDate">
+      <input type="date"
+        className="inputDate"
+        placeholder="Ajouter aller"
       />
-    </>
+      <input type="date"
+        className="inputDate"
+        placeholder="Ajouter retour"
+      />
+    </div>
   )
 }
